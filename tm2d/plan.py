Fixed template normalization issue

tm2d/plan.py
@@ -32,8 +32,7 @@ class Template:
                       pixel_size: float,
                       ctf_params: CTFParams = None,
                       template_count: int = 1,
-                      cmd_stream: vd.CommandStream = None,
-                      disable_ctf: bool = False) -> vd.RFFTBuffer:
+                      cmd_stream: vd.CommandStream = None) -> vd.RFFTBuffer:
         """
         This abstract method should return a buffer containing the sampled and filtered template in real space.   
         """
@@ -61,8 +60,7 @@ class Template:
             pixel_size=pixel_size,
             ctf_params=ctf_params,
             template_count=template_count,
-            cmd_stream=cmd_stream,
-            disable_ctf=disable_ctf
+            cmd_stream=cmd_stream
         )
     
     def _get_rotation_matricies(self, rotations: np.ndarray) -> np.ndarray:

tm2d/template_atomic.py
@@ -85,10 +85,22 @@ class TemplateAtomic(Template):
     shape: Tuple[int, int]
     atomic_coords: np.ndarray
     atomic_coords_buffer: vd.Buffer
+    disable_ctf: bool = False
+    disable_convolution: bool = False
+    disable_sigma_e: bool = False
 
-    def __init__(self, shape: Tuple[int, int], atomic_coords: np.ndarray):
+    def __init__(self,
+                 shape: Tuple[int, int],
+                 atomic_coords: np.ndarray,
+                 disable_ctf: bool = False,
+                 disable_convolution: bool = False,
+                 disable_sigma_e: bool = False):
         assert len(shape) == 2, "Shape must be a tuple of two integers (height, width)."
         assert atomic_coords.ndim == 2 and atomic_coords.shape[1] == 3, "Atomic coordinates must be a 2D array with shape (N, 3)."
+
+        self.disable_ctf = disable_ctf
+        self.disable_convolution = disable_convolution
+        self.disable_sigma_e = disable_sigma_e
 
         self.shape = (shape[0], shape[1])
         self.atomic_coords = atomic_coords.astype(np.float32)
@@ -99,8 +111,7 @@ class TemplateAtomic(Template):
                       pixel_size: float,
                       ctf_params: CTFParams,
                       template_count: int,
-                      cmd_stream: vd.CommandStream,
-                      disable_ctf: bool = False) -> vd.RFFTBuffer:
+                      cmd_stream: vd.CommandStream) -> vd.RFFTBuffer:
         
         sigma_e = tu.get_sigmaE(ctf_params.HT)
 
@@ -134,11 +145,23 @@ class TemplateAtomic(Template):
         fill_buffer(template_buffer)
         place_atoms(template_buffer, self.atomic_coords_buffer, rotations, pixel_size)
 
+        my_sigma_e = 1.0 if self.disable_sigma_e else sigma_e
+
+        if self.disable_convolution:
+            @vd.shader("buff.size * 2")
+            def map_int_to_float_shader(buff: Buff[f32]):
+                tid = vc.global_invocation().x
+                buff[tid] = vc.float_bits_to_int(buff[tid]).cast_to(vd.float32) * my_sigma_e
+
+            map_int_to_float_shader(template_buffer)
+
+            return template_buffer
+
         @vd.map_registers([c64])
         def map_int_to_float(buff: Buff[f32]):
             tid = vc.mapping_index()
 
-            value = vc.float_bits_to_int(buff[tid]).cast_to(vd.float32) * sigma_e
+            value = vc.float_bits_to_int(buff[tid]).cast_to(vd.float32) * my_sigma_e
 
             vc.mapping_registers()[0].x = value
             vc.mapping_registers()[0].y = 0.0
@@ -163,7 +186,7 @@ class TemplateAtomic(Template):
                 my_pixel_size
             )
 
-            if disable_ctf:
+            if self.disable_ctf:
                 return
 
             pos_2d = vc.new_vec2()

tm2d/template_density.py
@@ -109,10 +109,12 @@ class TemplateDensity(Template):
     shape: Tuple[int, int]
     density_array: np.ndarray
     density_image: vd.Image3D
+    disable_ctf: bool
 
-    def __init__(self, density_array: np.ndarray, transformed: bool = False):
+    def __init__(self, density_array: np.ndarray, transformed: bool = False, disable_ctf: bool = False):
         assert density_array.ndim == 3, "Density array must be a 3D array."
 
+        self.disable_ctf = disable_ctf
         self.density_array = density_array
 
         if transformed:
@@ -131,8 +133,7 @@ class TemplateDensity(Template):
                     rotations: vc.Var[vc.m4],
                     pixel_size: float,
                     defoci: List[vc.Var[vc.v4]],
-                    ctf_params: CTFParams,
-                    disable_ctf: bool = False) -> vd.RFFTBuffer:
+                    ctf_params: CTFParams) -> vd.RFFTBuffer:
         
         template_buffer_temp = vd.RFFTBuffer((len(defoci), *self.shape))
 
@@ -182,7 +183,7 @@ class TemplateDensity(Template):
 
             for i in range(template_buffer_temp.shape[0]):
                 index = ind + i * template_buffer_temp.shape[1] * template_buffer_temp.shape[2]
-                if disable_ctf:
+                if self.disable_ctf:
                     buff[index] = my_pos.xy
                 else:
                     buff[index] = my_pos.xy * ctf_filter(